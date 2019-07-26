c3b8995fc5ae41f97b23acf974b8d7b25469b6a3
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-26 09:56:21 -0700

    completed CharacterCard/Character List
    only need minor styling details

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,8 +4,11 @@ export default function CharacterCard(props) {
   return (
     <div>
       <img src={props.character.image} />
-      <p>{props.character.name}</p>
-      <p>{props.character.location.name}</p>
+      <p>
+        Name:{props.character.name} Species: {props.character.species}
+      </p>
+      <p>Location: {props.character.location.name}</p>
+      <p>Origin: {props.character.origin.name}</p>
     </div>
   );
 }
